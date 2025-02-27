35368c3bbe3050cf4ed3626d2461630b7b50f0ac
Sriram Chowdary <78009588+sriram-2490@users.noreply.github.com>
2025-02-27 17:41:53 -0600

    Update firebaseConfig.jsx

--- src/service/firebaseConfig.jsx
+++ src/service/firebaseConfig.jsx
@@ -1,23 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getFirestore} from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyDZffjPQBkG6wjLDLbDd4Y5bTQdn0N7N0Y",
-  authDomain: "travle-planner-6dbe3.firebaseapp.com",
-  projectId: "travle-planner-6dbe3",
-  storageBucket: "travle-planner-6dbe3.firebasestorage.app",
-  messagingSenderId: "1067958386408",
-  appId: "1:1067958386408:web:b66a486821c1f9281dd35d",
-  measurementId: "G-YWKN4700RP"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const db=getFirestore(app)
-// const analytics = getAnalytics(app);
+export const db = getFirestore(app);
